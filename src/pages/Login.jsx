8ca5220347d969172d8d4ab98d20388d756a83fb
Divyansh Gupta <135640318+DivyanshGupta-07@users.noreply.github.com>
2024-07-15 23:27:23 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/login", {
+      const response = await fetch("https://accredian-backend-task-yq5k.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
