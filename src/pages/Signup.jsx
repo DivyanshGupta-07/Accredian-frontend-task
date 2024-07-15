df116e560737eaa5909787591cf89af532870eb5
Divyansh Gupta <135640318+DivyanshGupta-07@users.noreply.github.com>
2024-07-15 23:27:46 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
       return;
     }
     try {
-      const response = await fetch("http://localhost:3000/api/signup", {
+      const response = await fetch("https://accredian-backend-task-yq5k.onrender.com/api/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
